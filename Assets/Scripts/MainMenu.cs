70d6dfd1598d2a02530674908a18dd72b4a6b201
zeilerphone <zeiler@ucsb.edu>
2024-05-10 13:24:01 -0700

    final fixes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement; // This is needed to use SceneManager
 
 public class MainMenu : MonoBehaviour
 {
+    public UnityEvent onPlay;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
